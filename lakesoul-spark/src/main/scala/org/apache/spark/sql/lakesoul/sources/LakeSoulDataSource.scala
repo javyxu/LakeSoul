6c7736bda692f937f78ec6a918bb6151bd20e223
zenghua <441651826@qq.com>
2022-08-17 11:38:22 +0800

    [Native-io][Spark]add debuginfo for parquetScan

--- lakesoul-spark/src/main/scala/org/apache/spark/sql/lakesoul/sources/LakeSoulDataSource.scala
+++ lakesoul-spark/src/main/scala/org/apache/spark/sql/lakesoul/sources/LakeSoulDataSource.scala
@@ -88,7 +88,7 @@ class LakeSoulDataSource
                               mode: SaveMode,
                               parameters: Map[String, String],
                               data: DataFrame): BaseRelation = {
-    println("[Debug][huazeng]log something")
+    logInfo("[Debug][huazeng]log something")
     val path = parameters.getOrElse("path", {
       throw LakeSoulErrors.pathNotSpecifiedException
     })
@@ -106,7 +106,7 @@ class LakeSoulDataSource
 
   override def createRelation(sqlContext: SQLContext,
                               parameters: Map[String, String]): BaseRelation = {
-    println("[Debug][huazeng]log something")
+    logInfo("[Debug][huazeng]log something")
     val path = parameters.getOrElse("path", {
       throw LakeSoulErrors.pathNotSpecifiedException
     })
@@ -122,7 +122,7 @@ class LakeSoulDataSource
   override def getTable(schema: StructType,
                         partitioning: Array[Transform],
                         properties: java.util.Map[String, String]): Table = {
-    println("[Debug][huazeng]log something")
+    logInfo("[Debug][huazeng]log something")
     val options = new CaseInsensitiveStringMap(properties)
     val path = options.get("path")
     if (path == null) throw LakeSoulErrors.pathNotSpecifiedException
